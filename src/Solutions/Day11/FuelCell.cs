7592d54e171d4703871de1023f2db6a55c923b63
Mattias Rudén <mattias@indiesoft.org>
2018-12-12 00:52:20 +0100

    Day 11 solutions

--- src/Solutions/Day11/FuelCell.cs
+++ src/Solutions/Day11/FuelCell.cs
@@ -7,12 +7,25 @@ namespace Day11
 {
     public class FuelCell
     {
-        public int RackId => Location.X + 10;
         public Point Location { get; }
+        public int PowerLevel { get; }
 
-        public FuelCell(Point location)
+        public FuelCell(Point location, int powerLevel)
         {
             Location = location;
+            PowerLevel = powerLevel;
+        }
+
+        public static FuelCell Create(Point location, int gridSerialNumber)
+        {
+            var rackId = location.X + 10;
+            var powerLevel = location.Y * rackId;
+            powerLevel += gridSerialNumber;
+            powerLevel *= rackId;
+            powerLevel = (powerLevel / 100) % 10;
+            powerLevel -= 5;
+
+            return new FuelCell(location, powerLevel);
         }
     }
 }
--- src/Solutions/Day11/Program.cs
+++ src/Solutions/Day11/Program.cs
@@ -1,12 +1,77 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
 
 namespace Day11
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            const int size = 300;
+            const int serialNumber = 3628;
+            var grid = CreateGrid(size, serialNumber);
+            var part1Answer = FindArea(grid, size, 3);
+            Console.WriteLine($"Coordinate: {part1Answer.Item1.Location.X},{part1Answer.Item1.Location.Y} (power level: {part1Answer.Item3})");
+
+            (FuelCell, int, int) max = (null, 0, 0);
+            for (var i = 1; i <= size; i++)
+            {
+                var result = FindArea(grid, size, i);
+                if (result.Item3 < 0) break;
+                if (result.Item3 > max.Item3)
+                    max = result;
+            }
+
+            Console.WriteLine($"Identifier: {max.Item1.Location.X},{max.Item1.Location.Y},{max.Item2}");
+            Console.ReadLine();
+        }
+
+        private static (FuelCell, int, int) FindArea(FuelCell[,] grid, int gridSize, int squareSize)
+        {
+            var maxLevel = int.MinValue;
+            FuelCell fuelCell = null;
+            for (var x = 0; x < gridSize - (squareSize + 1); x++)
+            {
+                for (var y = 0; y < gridSize - (squareSize + 1); y++)
+                {
+                    var powerLevel = TotalPowerLevel(grid, x, y, squareSize);
+                    if (powerLevel > maxLevel)
+                    {
+                        maxLevel = powerLevel;
+                        fuelCell = grid[x, y];
+                    }
+                }
+            }
+
+            return (fuelCell, squareSize, maxLevel);
+        }
+
+        private static int TotalPowerLevel(FuelCell[,] grid, int topLeftX, int topleftY, int squareSize)
+        {
+            var result = 0;
+            for (var x = topLeftX; x < topLeftX + squareSize; x++)
+            {
+                for (var y = topleftY; y < topleftY + squareSize; y++)
+                {
+                    result += grid[x, y].PowerLevel;
+                }
+            }
+            return result;
+        }
+
+        private static FuelCell[,] CreateGrid(int size, int input)
+        {
+            var grid = new FuelCell[size, size];
+            for (var x = 0; x < size; x++)
+            {
+                for (var y = 0; y < size; y++)
+                {
+                    grid[x, y] = FuelCell.Create(new Point(x+1, y+1), input);
+                }
+            }
+            return grid;
         }
     }
 }
