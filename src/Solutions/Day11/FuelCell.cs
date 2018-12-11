a54d158197c8f468587fabcd55cd1902a5fdd603
Mattias Rudén <mattias@indiesoft.org>
2018-12-11 16:14:57 +0100

    Draft for day 11

---
+++ src/Solutions/Day11/FuelCell.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Day11
+{
+    public class FuelCell
+    {
+        public int RackId => Location.X + 10;
+        public Point Location { get; }
+
+        public FuelCell(Point location)
+        {
+            Location = location;
+        }
+    }
+}
