8d01c0698a20ae347bdd05a8f6a1f43eeb684a66
Mattias Rudén <mattias@indiesoft.org>
2018-12-18 12:39:07 +0100

    Day 18 solutions

---
+++ src/Solutions/Day18/Area.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day18
+{
+    class Area
+    {
+        public int WoodedAcres => CountAcres('|');
+        public int Lumberyards => CountAcres('#');
+        public int ResourceValue => WoodedAcres * Lumberyards;
+        private readonly char[,] _acres;
+        private readonly int _height;
+        private readonly int _width;
+
+        private Area(char[,] acres)
+        {
+            _acres = acres;
+            _height = acres.GetLength(1);
+            _width = acres.GetLength(0);
+        }
+
+        public static Area Create(string[] input)
+        {
+            var acres = new char[input[0].Length, input.Length];
+            for (var y = 0; y < input.Length; y++)
+            {
+                for (var x = 0; x < input[y].Length; x++)
+                {
+                    acres[x, y] = input[y][x];
+                }
+            }
+            return new Area(acres);
+        }
+
+        public Area Transform()
+        {
+            var destination = new char[_width, _height];
+            for (var y = 0; y < _height; y++)
+            {
+                for (var x = 0; x < _width; x++)
+                {
+                    var acre = _acres[x, y];
+                    var adjacent = GetAdjacent(x, y);
+                    if (acre == '.')
+                    {
+                        destination[x, y] = adjacent.Count(a => a == '|') > 2 ? '|' : '.';
+                    } else if (acre == '|')
+                    {
+                        destination[x, y] = adjacent.Count(a => a == '#') > 2 ? '#' : '|';
+                    }
+                    else
+                    {
+                        destination[x, y] = (adjacent.Any(a => a == '#') && adjacent.Any(a => a == '|')) ? '#' : '.';
+                    }
+                }
+            }
+
+            return new Area(destination);
+        }
+
+        private char[] GetAdjacent(int x, int y)
+        {
+            var xEdge = _width - 1;
+            var yEdge = _height - 1;
+
+            var adjacent = new List<char>();
+            for (var row = y - 1; row <= y + 1; row++)
+            {
+                if (row < 0 || row > yEdge) continue;
+                for (var col = x - 1; col <= x + 1; col++)
+                {
+                    if (row == y && col == x) continue;
+                    if (col < 0 || col > xEdge) continue;
+
+                    adjacent.Add(_acres[col, row]);
+                }
+            }
+            return adjacent.ToArray();
+        }
+
+        private int CountAcres(char type)
+        {
+            var count = 0;
+            for (var y = 0; y < _height; y++)
+            {
+                for (var x = 0; x < _width; x++)
+                {
+                    if (_acres[x, y] == type) count++;
+                }
+            }
+            return count;
+        }
+
+
+        public void Write()
+        {
+            for (var y = 0; y < _height; y++)
+            {
+                for (var x = 0; x < _width; x++)
+                {
+                    Console.Write(_acres[x, y]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ src/Solutions/Day18/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using AdventOfCode.Common;
+
+namespace Day18
+{
+    class Program
+    {
+        static void Main()
+        {
+            var lines = Input.ReadRows();
+            var area = Area.Create(lines);
+            var part1Answer = CalculatePart1Answer(area);
+            Console.WriteLine($"Resource value after 10 minutes: {part1Answer}");
+
+            var part2Answer = CalculatePart2Answer(area);
+            Console.WriteLine($"Resource value after 1000000000 minutes: {part2Answer}");
+
+            Console.ReadLine();
+        }
+
+        private static int CalculatePart1Answer(Area area)
+        {
+            area.Write();
+
+            for (var i = 0; i < 10; i++)
+            {
+                area = area.Transform();
+                area.Write();
+            }
+
+            var part1Answer = area.WoodedAcres * area.Lumberyards;
+            return part1Answer;
+        }
+        private static int CalculatePart2Answer(Area area)
+        {
+            for (var i = 0; i < 1000; i++)
+            {
+                area = area.Transform();
+            }
+            var repeatingValues = new List<int>();
+            var resourceValue = area.ResourceValue;
+            while (!repeatingValues.Contains(resourceValue))
+            {
+                repeatingValues.Add(resourceValue);
+                area = area.Transform();
+                resourceValue = area.ResourceValue;
+            }
+
+            var remain = 1000000000 - 1000;
+            var index = remain % repeatingValues.Count;
+            return repeatingValues[index];
+        }
+    }
+}
