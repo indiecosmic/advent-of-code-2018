f9586031eb6aadf12aaf39dd6a5d06971f914bae
Mattias Rudén <mattias@indiesoft.org>
2018-12-23 22:35:04 +0100

    Day 22 part 2 solution

--- src/Solutions/Day22/Program.cs
+++ src/Solutions/Day22/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AdventOfCode.Common;
 
 namespace Day22
 {
@@ -6,11 +7,11 @@ namespace Day22
     {
         static void Main(string[] args)
         {
-            var depth = 510;
+            var depth = 3339;
             var targetX = 10;
-            var targetY = 10;
-            var width = 20;
-            var height = 20;
+            var targetY = 715;
+            var width = 50;
+            var height = 1000;
 
             var erosion = new int[width, height];
             var risk = new int[width, height];
@@ -26,6 +27,7 @@ namespace Day22
             var part2Answer = ShortestPath(risk, 0, 0, (x: targetX, y: targetY));
 
             Console.WriteLine($"Total risk level: {part1Answer}");
+            Console.WriteLine($"Fewest number of minutes: {part2Answer}");
             Console.ReadLine();
 
         }
@@ -56,7 +58,7 @@ namespace Day22
             return erosion % 3;
         }
 
-        static bool AllowedGear(int x, int y, int g, int[,] risk, int targetX, int targetY)
+        static bool AllowedGear(int x, int y, int g, int[,] risk)
         {
             switch (risk[x, y])
             {
@@ -81,7 +83,7 @@ namespace Day22
             var height = map.GetLength(1);
 
             var distances = new int[width, height, 3];
-            var visited = new bool[width, height, 3];
+            var queue = new PriorityQueue<(int x, int y, int g)>();
             for (var y = 0; y < height; y++)
             {
                 for (var x = 0; x < width; x++)
@@ -89,19 +91,14 @@ namespace Day22
                     for (var g = 0; g < 3; g++)
                     {
                         distances[x, y, g] = int.MaxValue;
-                        visited[x, y, g] = false;
                     }
                 }
             }
 
             distances[sourceX, sourceY, (int)Gear.Torch] = 0;
-            while (true)
+            queue.Enqueue((sourceX, sourceY, (int)Gear.Torch), 0);
+            while (queue.TryDequeueMin(out var region))
             {
-                var region = MinDistance(distances, visited);
-                if (region == (-1, -1, -1)) return distances[target.x, target.y, 2];
-
-                visited[region.x, region.y, region.g] = true;
-
                 SetDistance(region, (region.x + north.x, region.y + north.y, region.g));
                 SetDistance(region, (region.x + south.x, region.y + south.y, region.g));
                 SetDistance(region, (region.x + west.x, region.y + west.y, region.g));
@@ -109,71 +106,43 @@ namespace Day22
                 SetTool(region, 0);
                 SetTool(region, 1);
                 SetTool(region, 2);
-
             }
+            return distances[target.x, target.y, 2];
 
             void SetDistance((int x, int y, int g) source, (int x, int y, int g) destination)
             {
                 if (destination.x < 0 || destination.x >= width || destination.y < 0 ||
                     destination.y >= height) return;
 
-                //var travelCost = TravelCost(source, destination);
-                if (!AllowedGear(destination.x, destination.y, destination.g, map, target.x, target.y))
-                {
+                if (!AllowedGear(destination.x, destination.y, destination.g, map))
                     return;
-                }
 
-                if (!visited[destination.x, destination.y, destination.g] &&
-                    distances[source.x, source.y, source.g] != int.MaxValue &&
-                    distances[destination.x, destination.y, destination.g] + 1 < distances[source.x, source.y, source.g])
+                var distance = distances[source.x, source.y, source.g];
+                if (distance != int.MaxValue &&
+                    distances[destination.x, destination.y, destination.g] > distance + 1)
                 {
-                    distances[destination.x, destination.y, destination.g] = distances[source.x, source.y, destination.g] + 1;
+                    distances[destination.x, destination.y, destination.g] = distance + 1;
+                    queue.Enqueue((destination.x, destination.y, destination.g), distance + 1);
                 }
             }
 
-            void SetTool((int x, int y, int g) source, int g)
+            void SetTool((int x, int y, int g) source, int otherTool)
             {
-                if (source.g == g) return;
+                if (source.g == otherTool) return;
 
-                if (!AllowedGear(source.x, source.y, g, map, target.x, target.y))
-                {
-                    visited[source.x, source.y, g] = true;
+                if (!AllowedGear(source.x, source.y, otherTool, map))
                     return;
-                }
+
                 var d = distances[source.x, source.y, source.g];
 
-                if (distances[source.x, source.y, g] > d + 7)
+                if (distances[source.x, source.y, otherTool] > d + 7)
                 {
-                    distances[source.x, source.y, g] = 7 + d;
+                    distances[source.x, source.y, otherTool] = 7 + d;
+                    queue.Enqueue((source.x, source.y, otherTool), 7+d);
                 }
             }
         }
-
-        private static (int x, int y, int g) MinDistance(int[,,] distances, bool[,,] visited)
-        {
-            var width = distances.GetLength(0);
-            var height = distances.GetLength(1);
-            int min = int.MaxValue;
-            var minRegion = (-1, -1, -1);
-
-            for (var y = 0; y < height; y++)
-            {
-                for (var x = 0; x < width; x++)
-                {
-                    for (var g = 0; g < 3; g++)
-                    {
-                        if (visited[x, y, g] == false && distances[x, y, g] <= min)
-                        {
-                            min = distances[x, y, g];
-                            minRegion = (x, y, g);
-                        }
-                    }
-                }
-            }
-
-            return minRegion;
-        }
-
+        
         enum Gear
         {
             None = 0,
