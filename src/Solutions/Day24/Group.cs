5f5621eeb1d486c227dfc0ccfd7f0ee8418e670b
Mattias Rudén <mattias@indiesoft.org>
2018-12-25 01:41:33 +0100

    Day 24 solutions

---
+++ src/Solutions/Day24/Group.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Day24
+{
+    class Group
+    {
+        public int GroupNumber { get; }
+        public int Units { get; private set; }
+        public int AttackDamage { get; private set; }
+        public int Initiative { get; }
+        public int HitPoints { get; }
+        public string AttackType { get; }
+        public List<string> Immunities { get; }
+        public List<string> Weaknesses { get; }
+        public int EffectivePower => Units * AttackDamage;
+        public Faction Faction { get; }
+        public bool CanAttack => Units > 0;
+
+        private Group(int groupNumber, int units, int attackDamage, int initiative, int hitPoints, string attackType,
+            Faction faction, List<string> immunities, List<string> weaknesses)
+        {
+            GroupNumber = groupNumber;
+            Units = units;
+            AttackDamage = attackDamage;
+            Initiative = initiative;
+            HitPoints = hitPoints;
+            AttackType = attackType;
+            Faction = faction;
+            Immunities = immunities;
+            Weaknesses = weaknesses;
+        }
+
+        public int PotentialDamage(Group attacker)
+        {
+            if (Immunities.Contains(attacker.AttackType)) return 0;
+            var multiplier = Weaknesses.Contains(attacker.AttackType) ? 2 : 1;
+            var damage = multiplier * attacker.EffectivePower;
+
+            return damage;
+        }
+
+        public Group SelectTarget(IEnumerable<Group> potentialTargets)
+        {
+            return potentialTargets.OrderByDescending(g => (g.PotentialDamage(this), g.EffectivePower, g.Initiative))
+                .FirstOrDefault();
+        }
+
+        public int Attack(Group target)
+        {
+            var originalUnits = target.Units;
+            var damage = target.PotentialDamage(this);
+            target.DealDamage(damage);
+            //if (Faction == Faction.ImmuneSystem)
+            //    Console.ForegroundColor = ConsoleColor.Green;
+            //else
+            //    Console.ForegroundColor = ConsoleColor.Red;
+
+            //Console.WriteLine($"{Faction} group {GroupNumber} attacks {target.Faction} group {target.GroupNumber} with {damage} damage, killing {originalUnits - target.Units} units");
+            //Console.ForegroundColor = ConsoleColor.White;
+            return originalUnits - target.Units;
+        }
+
+        private void DealDamage(int damage)
+        {
+            var unitsLost = damage / HitPoints;
+            Units -= unitsLost;
+            if (Units < 0) Units = 0;
+        }
+
+        public void Boost(int value)
+        {
+            AttackDamage += value;
+        }
+
+        public override string ToString()
+        {
+            return
+                $"{Units} units each with {HitPoints} hit points(weak to {string.Join(",", Weaknesses)}; immune to {string.Join(",", Immunities)}) with an attack that does {AttackDamage} {AttackType} damage at initiative {Initiative}";
+        }
+
+        public static Group Parse(string input, Faction faction, int groupNumber)
+        {
+            var matches = Regex.Matches(input,
+                @"(?<units>\d+) units|(?<hp>\d+) hit points|weak to (?<weak>[\w,\s]+)|immune to (?<immune>[\w,\s]+)|initiative (?<initiative>\d+)|does (?<dmg>\d+)|(?<type>\b\w+\b) damage");
+            if (matches.Count == 0) throw new ArgumentException($"Invalid input: {input}", nameof(input));
+            var immunities = "";
+            var weaknesses = "";
+            var attackDamage = 0;
+            var hitPoints = 0;
+            var initiative = 0;
+            var attackType = "";
+            var units = 0;
+            foreach (Match match in matches)
+            {
+                if (match.Groups["dmg"].Success)
+                    attackDamage = int.Parse(match.Groups["dmg"].Value);
+                else if (match.Groups["hp"].Success)
+                    hitPoints = int.Parse(match.Groups["hp"].Value);
+                else if (match.Groups["immune"].Success)
+                    immunities = match.Groups["immune"].Value;
+                else if (match.Groups["weak"].Success)
+                    weaknesses = match.Groups["weak"].Value;
+                else if (match.Groups["initiative"].Success)
+                    initiative = int.Parse(match.Groups["initiative"].Value);
+                else if (match.Groups["type"].Success)
+                    attackType = match.Groups["type"].Value;
+                else if (match.Groups["units"].Success)
+                    units = int.Parse(match.Groups["units"].Value);
+            }
+
+            var weaknessesList = new List<string>();
+            if (!string.IsNullOrEmpty(weaknesses))
+            {
+                weaknessesList.AddRange(weaknesses.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            }
+            var immunitiesList = new List<string>();
+            if (!string.IsNullOrEmpty(immunities))
+            {
+                immunitiesList.AddRange(immunities.Split(new[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            }
+
+            return new Group(groupNumber, units, attackDamage, initiative, hitPoints, attackType, faction, immunitiesList, weaknessesList);
+        }
+    }
+    public enum Faction
+    {
+        None,
+        ImmuneSystem,
+        Infection
+    }
+}
---
+++ src/Solutions/Day24/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AdventOfCode.Common;
+
+namespace Day24
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Input.ReadRows();
+            
+            var factions = CreateFactions(input);
+            var part1Answer = SimulateBattle(factions.immuneSystem, factions.infection);
+            Console.WriteLine($"{part1Answer.winner} wins. Total remaining units {part1Answer.unitsRemaining}");
+
+            var part2Answer = SimulateBattleWithBoost(input);
+            Console.WriteLine($"{part2Answer.winner} wins. Total remaining units {part2Answer.unitsRemaining}");
+
+            Console.ReadLine();
+        }
+
+        private static (Faction winner, int unitsRemaining) SimulateBattleWithBoost(string[] input)
+        {
+            var minBoost = 0;
+            var maxBoost = 5000;
+            (Faction winner, int unitsRemaining) outcome;
+            while (maxBoost - minBoost > 1)
+            {
+                var boost = (maxBoost + minBoost) / 2;
+                var factions = CreateFactions(input);
+                foreach (var group in factions.immuneSystem)
+                    group.Boost(boost);
+
+                outcome = SimulateBattle(factions.immuneSystem, factions.infection);
+                if (outcome.winner == Faction.ImmuneSystem)
+                {
+                    maxBoost = boost;
+                }
+                else
+                {
+                    minBoost = boost;
+                }
+            }
+
+            var lastRound = CreateFactions(input);
+            foreach (var group in lastRound.immuneSystem)
+                group.Boost(maxBoost);
+
+            outcome = SimulateBattle(lastRound.immuneSystem, lastRound.infection);
+
+            return outcome;
+        }
+
+        private static (Faction winner, int unitsRemaining) SimulateBattle(List<Group> immuneSystem, List<Group> infection)
+        {
+            var attackOrder = new PriorityQueue<(Group attacker, Group target)>();
+            var targets = new HashSet<Group>();
+            while (immuneSystem.Count > 0 && infection.Count > 0)
+            {
+                // Target selection
+                var selectionOrder = immuneSystem.Union(infection).OrderByDescending(g => (g.EffectivePower, g.Initiative)).ToList();
+                foreach (var attacker in selectionOrder)
+                {
+                    var target = attacker.Faction == Faction.ImmuneSystem
+                        ? attacker.SelectTarget(infection.Where(g => !targets.Contains(g)))
+                        : attacker.SelectTarget(immuneSystem.Where(g => !targets.Contains(g)));
+                    if (target != null && target.PotentialDamage(attacker) > 0)
+                    {
+                        targets.Add(target);
+                        attackOrder.Enqueue((attacker, target), attacker.Initiative);
+                    }
+                }
+
+
+                var stalemateCount = 0;
+                var attackCount = attackOrder.Count;
+                // Attack phase
+                while (attackOrder.TryDequeueMax(out var battle))
+                {
+                    if (battle.attacker.CanAttack) { 
+                        var damageDealt = battle.attacker.Attack(battle.target);
+                        if (damageDealt == 0) stalemateCount++;
+                    }
+                }
+
+                if (stalemateCount == attackCount)
+                {
+                    return (Faction.None, 0);
+                }
+
+                immuneSystem = immuneSystem.Where(i => i.Units > 0).ToList();
+                infection = infection.Where(i => i.Units > 0).ToList();
+                targets.Clear();
+            }
+
+            var winner = immuneSystem.Count > 0 ? immuneSystem : infection;
+            var totalUnits = winner.Sum(w => w.Units);
+            var winningFaction = winner.First().Faction;
+            return (winningFaction, totalUnits);
+        }
+
+        static (List<Group> immuneSystem, List<Group> infection) CreateFactions(string[] input)
+        {
+            var immuneSystem = new List<Group>();
+            var infection = new List<Group>();
+            var currentGroup = immuneSystem;
+            var currentFaction = Faction.ImmuneSystem;
+            var groupNumber = 0;
+            foreach (var line in input)
+            {
+                if (line.StartsWith("Immune"))
+                {
+                    currentGroup = immuneSystem;
+                    currentFaction = Faction.ImmuneSystem;
+                    groupNumber = 0;
+                }
+                else if (line.StartsWith("Infection"))
+                {
+                    currentGroup = infection;
+                    currentFaction = Faction.Infection;
+                    groupNumber = 0;
+                }
+                else
+                {
+                    currentGroup.Add(Group.Parse(line, currentFaction, groupNumber));
+                }
+
+                groupNumber++;
+            }
+
+            return (immuneSystem, infection);
+        }
+    }
+}
